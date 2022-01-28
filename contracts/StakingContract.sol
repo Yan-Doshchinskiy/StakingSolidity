5a7841b97d57cee2c97866d2a5893f05d83971b5
Yan.Doshinskiy <yan.doshinskiy@softlex.pro>
2022-01-28 09:47:34 +0700

    gitignore

--- contracts/StakingContract.sol
+++ contracts/StakingContract.sol
@@ -1,11 +1,11 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract ShitcoinToken {
-    // structs
+    // Staker struct
     struct Staker {
         uint256 amount;
         uint256 distributed;
@@ -13,15 +13,24 @@ contract ShitcoinToken {
         uint256 missed;
     }
 
+    // stakers mapping
+    mapping(address => Staker) private stakers;
+
+    // roles
+    bytes32 public ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
+    // tokens
+    IERC20 private stakingToken;
+    IERC20 private rewardsToken;
+
     // variables
     uint256 private startTime;
     uint256 private producedTime;
     address private treasury;
-    IERC20 private stakingToken;
-    IERC20 private rewardsToken;
-    mapping(address => Staker) private stakers;
+
 
     constructor(uint256 _startTime, address _treasury, address _stakingToken, address _rewardsToken) {
+        _setupRole(ADMIN_ROLE, msg.sender);
         startTime = _startTime;
         producedTime = _startTime;
         treasury = _treasury;
